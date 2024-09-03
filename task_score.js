99716f97a5dd4a25d0a3d4f5d03af875e519329d
Yuliya Banko <yuliyabanko@gmail.com>
2024-09-03 13:32:09 +0300

    Homework_2.1.4 Выведите имя в формате «A. Einstein»

--- task_score.js
+++ task_score.js
@@ -5,11 +5,15 @@ let git = 30;
 let js = 60;
 let total = vvedenie + git + js; 
 let score = total/3;
-
 console.log(`This is the HWscore ${score}.`)
 
 let stName = "Ilon";
 let stSurname = "Mask";
 let fullName = stName + " " + stSurname;
+console.log(`This is the score of our student ${fullName} ${score}.`);
 
-console.log(`This is the score of our student ${fullName} ${score}.`)
+let nameLenght = stName.length;
+console.log(`This is the score of our student ${stName.charAt(0)}.${stSurname} ${score}.`);
+
+let stName1 = ['I', 'l', 'o','n'];
+console.log(`This is the score of our student ${stName1.slice(0,1)}.${stSurname} ${score}.`);
